e34725050d8eb671594820acb95766780a6f16d3
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2024-12-29 00:09:10 -0800

    getters/setters for player model

--- src/main/java/com/jeremiahsoe/craftlytics/model/Player.java
+++ src/main/java/com/jeremiahsoe/craftlytics/model/Player.java
@@ -1,6 +1,8 @@
 package com.jeremiahsoe.craftlytics.model;
 
 import jakarta.persistence.*;
+import org.springframework.cglib.core.Local;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,5 +17,41 @@ public class Player {
     private String playerName;
     private LocalDateTime firstSeen;
     private LocalDateTime lastSeen;
+
+    //GETTERS
+    public Long getId(){
+        return this.id;
+    }
+    public String getPlayerUuid(){
+        return this.playerUuid;
+    }
+    public String getPlayerName(){
+        return this.playerName;
+    }
+    public LocalDateTime getFirstSeen(){
+        return this.firstSeen;
+    }
+    public LocalDateTime getLastSeen(){
+        return this.lastSeen;
+    }
+
+    //SETTERS
+    public void setId(Long id){
+        this.id = id;
+    }
+    public void setPlayerUuid(String playerUuid){
+        this.playerUuid = playerUuid;
+    }
+    public void setPlayerName(String playerName){
+        this.playerName = playerName;
+    }
+    public void setFirstSeen(LocalDateTime firstSeen){
+        this.firstSeen = firstSeen;
+    }
+    public void setLastSeen(LocalDateTime lastSeen){
+        this.lastSeen = lastSeen;
+    }
+
+
 
 }
