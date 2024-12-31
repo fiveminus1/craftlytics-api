4f297484d095cc04cbc31855b057763da8c16ec3
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2024-12-30 22:39:42 -0800

    enabled cors for testing

--- src/main/java/com/jeremiahsoe/craftlytics_api/controller/PlayerController.java
+++ src/main/java/com/jeremiahsoe/craftlytics_api/controller/PlayerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/players")
+@CrossOrigin(origins="http://localhost:3000")
 public class PlayerController {
     @Autowired
     private PlayerService playerService;
